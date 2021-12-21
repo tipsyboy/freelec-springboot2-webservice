ea116423dfcd4fada35bd63eaba486ca43fdb2ed
TipsyBoy <gudah13@naver.com>
2021-12-21 21:00:40 +0900

    Entity class 작성과 spring-data-jpa 인터페이스 작성

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+
+    // 구글링 - JAVA Builder pattern 공부
+    @Builder
+    public Posts(String title, String content, String author) { // 선언된 필드들만 빌더에 포함함
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
