4563647b7df28415e7d7997e01e448c2cac3f8dc
TipsyBoy <gudah13@naver.com>
2021-12-22 21:08:41 +0900

    BaseTimeEntity를 추상클래스로 변경

--- src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass // JPA Entity들이 class를 상속할때, 상속 필드들도 컬럼으로 인식할 수 있게함. (필드 상속)
 @EntityListeners(AuditingEntityListener.class) // Auditing 기능 - 구글링~
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdDate;
