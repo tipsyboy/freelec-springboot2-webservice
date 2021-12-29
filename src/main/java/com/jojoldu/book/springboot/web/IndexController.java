a6499abbfe49fb6dba3ba5a6119559ad5cf6933b
TipsyBoy <gudah13@naver.com>
2021-12-29 15:05:13 +0900

    어노테이션 기반으로 개선

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,23 +1,33 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model 서버 템플릿 엔진으로 보낼 객체를 사용할 수 있음.
+    public String index(Model model, @LoginUser SessionUser user) { // Model 서버 템플릿 엔진으로 보낼 객체를 사용할 수 있음.
         model.addAttribute("posts", postsService.findAllDesc());
+
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
